5c510d99124f7e41451a20ff5c109ba9ae8bac7d
kai-niemi <kai.niemi.se@gmail.com>
2023-03-13 18:15:31 +0100

    Prepare 1.0.0 release

--- cockroachdb-jdbc-driver/src/main/java/io/cockroachdb/jdbc/CockroachDriverInfo.java
+++ cockroachdb-jdbc-driver/src/main/java/io/cockroachdb/jdbc/CockroachDriverInfo.java
@@ -10,7 +10,7 @@ public abstract class CockroachDriverInfo {
     // Driver name
     public static final String DRIVER_NAME = "CockroachDB JDBC Driver";
 
-    public static final String DRIVER_VERSION = "0.9.4-SNAPSHOT";
+    public static final String DRIVER_VERSION = "1.0.0";
 
     public static final String DRIVER_FULL_NAME = DRIVER_NAME + " " + DRIVER_VERSION;
 
